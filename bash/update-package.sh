a5b50b7877c350e8d3a88b4c6e2bf0c2a2230490
trapcodeio <trapcode.io@gmail.com>
2023-02-20 18:49:02 +0100

    Update css

--- bash/update-package.sh
+++ bash/update-package.sh
@@ -8,7 +8,8 @@ rm -rf ./package/src
 cp -r ./src/components/debug ./package/src
 
 # delete tailwind source code
-rm -rf ./package/src/tailwind.css
+rm -rf ./package/src/tailwind.scss
+rm -rf ./package/src/tailwind.css.map
 
 # Build debug.css
 npx tailwindcss -i ./src/components/debug/tailwind.css -o ./package/src/debug.css --minify
