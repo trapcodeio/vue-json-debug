4f8e1b00aa8e51ef56ae14c4a44a0d272714820f
trapcodeio <trapcode.io@gmail.com>
2024-01-02 20:34:09 +0100

    update

--- src/components/debug/docks/RouterInfo.vue
+++ src/components/debug/docks/RouterInfo.vue
@@ -1,3 +1,7 @@
+<script setup>
+import { useRoute } from "vue-router";
+const $route = useRoute();
+</script>
 <template>
   <div v-if="$route && $route.name" class="vdd-container vjd-space-x-1">
     <small class="vdd-label">RouteName:</small>
